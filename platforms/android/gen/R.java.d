9844688e6418c1c1ec7488ad6224435efdaee04e
eb1 <ebrommers@gmail.com>
2013-10-03 15:47:49 -0500

    Rebuild Android platform on Mac

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+/Users/erik/dev/adapt-it-mobile/platforms/android/gen/org/sil/aimobile/R.java \
+ : /Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable/icon.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-hdpi/ic_launcher.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-hdpi/icon.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-ldpi/ic_launcher.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-ldpi/icon.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-mdpi/ic_launcher.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-mdpi/icon.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-xhdpi/ic_launcher.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/layout/main.xml \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/values/strings.xml \
+/Users/erik/dev/adapt-it-mobile/platforms/android/res/xml/config.xml \
+/Users/erik/dev/adapt-it-mobile/platforms/android/bin/AndroidManifest.xml \
