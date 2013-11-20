07a916b4e4e81833616d867596be2a17223c1c58
eb1 <ebrommers@gmail.com>
2013-11-20 17:22:40 -0600

    use linux for android platform

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/home/erikb/dev/adapt-it-mobile/platforms/android/gen/org/sil/aimobile/R.java \
+ : /home/erikb/dev/adapt-it-mobile/platforms/android/res/drawable-hdpi/icon.png \
+/home/erikb/dev/adapt-it-mobile/platforms/android/res/drawable-xhdpi/icon.png \
+/home/erikb/dev/adapt-it-mobile/platforms/android/res/values/strings.xml \
+/home/erikb/dev/adapt-it-mobile/platforms/android/res/drawable-ldpi/icon.png \
+/home/erikb/dev/adapt-it-mobile/platforms/android/res/xml/config.xml \
+/home/erikb/dev/adapt-it-mobile/platforms/android/res/drawable-mdpi/icon.png \
+/home/erikb/dev/adapt-it-mobile/platforms/android/res/drawable/icon.png \
+/home/erikb/dev/adapt-it-mobile/platforms/android/bin/AndroidManifest.xml \
---
+++ platforms/android/gen/org/sil/aimobile/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.sil.aimobile;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
