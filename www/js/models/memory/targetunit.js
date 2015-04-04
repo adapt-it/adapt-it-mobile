e72aba06c805d1572b69144591b69aead5726d8e
eb1 <ebrommers@gmail.com>
2015-04-04 15:58:54 -0500

    sql persistence

--- www/js/models/memory/targetunit.js
+++ www/js/models/memory/targetunit.js
@@ -9,24 +9,19 @@ define(function (require) {
         i           = 0,
         targetunits = [],
         
-        findById = function (id) {
-            var deferred = $.Deferred(),
-                targetunit = null,
-                l = targetunits.length;
-            for (i = 0; i < l; i++) {
-                if (targetunits[i].id === id) {
-                    targetunit = targetunits[i];
-                    break;
-                }
-            }
-            deferred.resolve(targetunit);
+        findById = function (searchKey) {
+            var deferred = $.Deferred();
+            var results = sourcephrases.filter(function (element) {
+                return element.attributes.tuid.toLowerCase().indexOf(searchKey.toLowerCase()) > -1;
+            });
+            deferred.resolve(results);
             return deferred.promise();
         },
 
-        findByName = function (searchKey) {
+        findByProjectId = function (searchKey) {
             var deferred = $.Deferred();
-            var results = targetunits.filter(function (element) {
-                return element.id.toLowerCase().indexOf(searchKey.toLowerCase()) > -1;
+            var results = sourcephrases.filter(function (element) {
+                return element.attributes.projectid.toLowerCase().indexOf(searchKey.toLowerCase()) > -1;
             });
             deferred.resolve(results);
             return deferred.promise();
@@ -43,7 +38,7 @@ define(function (require) {
 
         TargetUnit = Backbone.Model.extend({
             defaults: {
-                id: 0,
+                tuid: 0,
                 projectid: 0,
                 source: "",
                 refstring: [],
@@ -58,7 +53,7 @@ define(function (require) {
             fetch: function () {
                 var attributes = this.attributes;
                 window.Application.db.transaction(function (tx) {
-                    tx.executeSql("SELECT * from chapter WHERE chapterid=?;", [attributes.chapterid], function (tx, res) {
+                    tx.executeSql("SELECT * from targetunit WHERE tuid=?;", [attributes.tuid], function (tx, res) {
                         console.log("SELECT ok: " + res.rows);
                         this.set(res.rows.item(0));
                     });
@@ -71,12 +66,12 @@ define(function (require) {
                 // is there a record already?
                 var attributes = this.attributes;
                 window.Application.db.transaction(function (tx) {
-                    tx.executeSql("SELECT COUNT(id) AS cnt FROM chapter WHERE chapterid=?;", [attributes.chapterid], function (tx, res) {
+                    tx.executeSql("SELECT COUNT(id) AS cnt FROM targetunit WHERE tuid=?;", [attributes.tuid], function (tx, res) {
                         console.log("SELECT ok: " + res.toString());
                         if (res.rows.item(0).cnt > 0) {
                             // there's already a record for this id -- update the values
                             window.Application.db.transaction(function (tx) {
-                                tx.executeSql("UPDATE chapter SET bookid=?, name=?, lastadapted=?, versecount=? WHERE chapterid=?;", [attributes.bookid, attributes.name, attributes.lastadapted, attributes.versecount, attributes.chapterid], function (tx, res) {
+                                tx.executeSql("UPDATE chapter SET projectid=?, source=?, refstring=?, timestamp=?, user=? WHERE tuid=?;", [attributes.projectid, attributes.source, attributes.refstring, attributes.timestamp, attributes.user, attributes.tuid], function (tx, res) {
                                     console.log("UPDATE ok: " + res.toString());
                                 });
                             }, function (err) {
@@ -85,7 +80,7 @@ define(function (require) {
                         } else {
                             // new record -- insert
                             window.Application.db.transaction(function (tx) {
-                                tx.executeSql("INSERT INTO chapter (chapterid,bookid,name,lastadapted,versecount) VALUES (?,?,?,?,?);", [attributes.chapterid, attributes.bookid, attributes.name, attributes.lastadapted, attributes.versecount], function (tx, res) {
+                                tx.executeSql("INSERT INTO targetunit (tuid,projectid,source,refstring,timestamp,user) VALUES (?,?,?,?,?,?);", [attributes.tuid, attributes.projectid, attributes.source, attributes.refstring, attributes.timestamp, attributes.user], function (tx, res) {
                                     console.log("INSERT ok: " + res.toString());
                                 });
                             }, function (err) {
@@ -100,7 +95,7 @@ define(function (require) {
 
             destroy: function (options) {
                 window.Application.db.transaction(function (tx) {
-                    tx.executeSql("DELETE FROM chapter WHERE chapterid=?;", [this.attributes.chapterid], function (tx, res) {
+                    tx.executeSql("DELETE FROM targetunit WHERE tuid=?;", [this.attributes.tuid], function (tx, res) {
                         console.log("DELETE ok: " + res.toString());
                     }, function (tx, err) {
                         console.log("DELETE error: " + err.toString());
@@ -146,8 +141,10 @@ define(function (require) {
                         for (i = 0, len = res.rows.length; i < len; ++i) {
                             // add the chapter
                             var tu = new TargetUnit();
+                            tu.off("change");
                             tu.set(res.rows.item(i));
                             targetunits.push(tu);
+                            tu.on("change", tu.save, tu);
                         }
                         console.log("SELECT ok: " + res.rows.length + " targetunit items");
                     });
@@ -162,9 +159,19 @@ define(function (require) {
 
             sync: function (method, model, options) {
                 if (method === "read") {
-                    findByName(options.data.name).done(function (data) {
-                        options.success(data);
-                    });
+                    if (options.data.hasOwnProperty('id')) {
+                        findById(options.data.id).done(function (data) {
+                            options.success(data);
+                        });
+                    } else if (options.data.hasOwnProperty('projectid')) {
+                        findByProjectId(options.data.projectid).done(function (data) {
+                            options.success(data);
+                        });
+                    } else if (options.data.hasOwnProperty('source')) {
+                        findBySource(options.data.source).done(function (data) {
+                            options.success(data);
+                        });
+                    }
                 }
             }
 
