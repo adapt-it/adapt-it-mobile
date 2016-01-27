7bc5e4d13b8e66beda32e1ea1773ebe097afc6ed
eb1 <ebrommers@gmail.com>
2016-01-27 10:07:39 -0600

    testing... again
    Hopefully this addresses the problem with AIM not installing on Kevin’s
    laptop.

--- www/js/utils/pageslider.js
+++ www/js/utils/pageslider.js
@@ -9,7 +9,7 @@ define(function (require) {
 
     return function PageSlider(container) {
 
-        var container = container,
+        var theContainer = container,
             currentPage,
             stateHistory = [];
 
@@ -43,7 +43,7 @@ define(function (require) {
             
 //            console.log("slidePageFrom: " + page + ", " + from);
 
-            container.append(page);
+            theContainer.append(page);
 
             if (!currentPage || !from) {
                 page.attr("class", "page page-center");
@@ -59,9 +59,29 @@ define(function (require) {
                 $(e.target).remove();
             });
 
+            // EDB BUGBUG 1/27/2016:
+            // Caught between a rock and a hard place.
+            // Issue: the animation here doesn't work in older Android browsers. 
+            // Details: Forcing the reflow does create the animation of sliding pages, 
+            //          but on older browsers the page slides mostly offscreen (see 
+            //          our issue #73). I'm unsure of where the problem lies, as it 
+            //          does work properly on newer browsers - caniuse.com states that
+            //          3d transforms are supported on older versions of the Android
+            //          browser, but I've seen a few mentions that older Android 
+            //          devices don't support 3d trandsforms in hardware.
+            // Workaround that broke: we initially went with the crosswalk browser plugin,
+            //          that replaces the normal webview with the latest Chromium browser.
+            //          This created more headaches than it solved -- multiple .apk files
+            //          for x86 and AMD, but more of sn issue, it didn't work on several
+            //          Android devices. We had to pull it.
+            // Current workaround: remove crosswalk AND remove the force reflow here. The 
+            //          implication is that we don't get a nice animation, but it'll work
+            //          on all browsers. This should be revisited to see if we can't get
+            //          at least SOME animation to work.
             // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
-            container[0].offsetWidth;
-
+//            var temp = theContainer[0].offsetWidth;
+            // end EDB BUGBUG
+            
             // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
             page.attr("class", "page transition page-center");
             currentPage.attr("class", "page transition " + (from === "page-left" ? "page-right" : "page-left"));
