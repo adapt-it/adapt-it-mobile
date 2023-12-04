250bc333eef25700af15ef62b017568b4a91f99a
Erik Brommers <ebrommers@gmail.com>
2023-12-04 13:55:57 -0800

    CI build stuff
    Command line to add ios and android platforms working; also set the default gulpfile task to just build Android (for the CI build)

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
 // Gulpfile for Adapt It Mobile builds
 var gulp = require("gulp"),
     fs = require("fs"),
+    cp = require('child_process'),
     cordova = require("cordova-lib").cordova;
 
-// call "cordova platform add android" if needed
+// prep the Android platform -- either add it or clean it
 gulp.task("prep-android-dir", function (done) {
-    var path = "platforms/android";
-    // process.stdout.write('prep-android-dir - doing nothing\n');
-    // done();
+    var path = "./platforms/android";
     if (!fs.existsSync(path)) {
         process.stdout.write('Android dir not detected -- creating platform android\n');
-        cordova.platforms("add", "android");
-        done();
+        var cmd = cp.spawn('cordova', ["platform", "add", "android"], {stdio: 'inherit'}).on('exit', done);
     } else {
         process.stdout.write('cleaning platform android\n');
         cordova.clean("android");
@@ -19,6 +17,7 @@ gulp.task("prep-android-dir", function (done) {
     }
 });
 
+// build Android
 gulp.task("build-android", function (done) {
     cordova.build({
         "platforms": ["android"],
@@ -28,15 +27,12 @@ gulp.task("build-android", function (done) {
     }, done());
 });
 
-// call "cordova platform add ios" if needed
+// prep the iOS platform -- either add it or clean it
 gulp.task("prep-ios-dir", function (done) {
-    var path = "platforms/ios";
-    // process.stdout.write('prep-ios-dir - doing nothing\n');
-    // done();
+    var path = "./platforms/ios";
     if (!fs.existsSync(path)) {
         process.stdout.write('ios dir not detected -- creating platform ios\n');
-        cordova.platforms("add", "ios");
-        done();
+        var cmd = cp.spawn('cordova', ["platform", "add", "ios@latest"], {stdio: 'inherit'}).on('exit', done);
     } else {
         process.stdout.write('cleaning platform ios\n');
         cordova.clean("ios");
@@ -44,6 +40,7 @@ gulp.task("prep-ios-dir", function (done) {
     }
 });
 
+// build iOS
 gulp.task("build-ios", function (done) {
     cordova.build({
         "platforms": ["ios"],
@@ -53,10 +50,14 @@ gulp.task("build-ios", function (done) {
     }, done());
 });
 
+// prep both Android and iOS
+gulp.task("prep", gulp.parallel("prep-android-dir", "prep-ios-dir"));
+
+// build both Android and iOS
 gulp.task("build", gulp.parallel("build-android", "build-ios"));
 
-gulp.task("default", gulp.series("build"), function () {
+// default (gulp) - just build Android for the CI build
+gulp.task("default", gulp.series("build-android"), function () {
     // Copy results to bin folder
     gulp.src("platforms/android/app/build/output/apk/release/*.apk").pipe(gulp.dest("bin/release/android"));         // Gradle build
-    // gulp.src("platforms/ios/build/device/*.ipa").pipe(gulp.dest("bin/release/ios"));
 });
