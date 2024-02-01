55e83c4a8b41b83c89e09c5de049aa367a2931d1
Erik Brommers <ebrommers@gmail.com>
2024-02-01 09:57:01 -0800

    A couple more socialsharing plugin removals
    Missed a call in the startup code

--- www/js/Application.js
+++ www/js/Application.js
@@ -191,13 +191,6 @@ define(function (require) {
                         cordova.file.syncedDataDirectory
                     ];
                 }
-                // social sharing plugin / iPad popover coords
-                if (device && (device.platform !== "browser")) {
-                    window.plugins.socialsharing.iPadPopupCoordinates = function () {
-                        var rect = document.getElementById('share_button').getBoundingClientRect();
-                        return rect.left + "," + rect.top + "," + rect.width + "," + rect.height;
-                    };
-                }
                 // create / open the database
                 if (device && (device.platform !== "browser")) {
                     if (device.platform === "browser") {
